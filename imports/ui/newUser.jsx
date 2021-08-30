7aacfb42594e788c9406d07fcd02d9b13c3a87b6
victor Ferreira <victorferreira.contacto@gmail.com>
2021-08-30 16:09:22 -0300

    First Commit

---
+++ imports/ui/newUser.jsx
@@ -0,0 +1,12 @@
+import { Meteor } from 'meteor/meteor';
+import React, {useState} from 'react';
+import {tasksMethods} from '/imports/api/tasksMethods';
+import { LoginForm } from './LoginForm';
+
+
+export const newUser = () =>{
+    return (
+        <div>Working</div>
+    );
+}
+
---
+++ tests/main.js
@@ -0,0 +1,23 @@
+import assert from "assert";
+import '/imports/api/tasksMethods.tests.js';
+import '/imports/api/tasks.tests.js';
+
+
+describe("simple-todo-react", function () {
+  it("package.json has correct name", async function () {
+    const { name } = await import("../package.json");
+    assert.strictEqual(name, "simple-todo-react");
+  });
+
+  if (Meteor.isClient) {
+    it("client is not server", function () {
+      assert.strictEqual(Meteor.isServer, false);
+    });
+  }
+
+  if (Meteor.isServer) {
+    it("server is not client", function () {
+      assert.strictEqual(Meteor.isClient, false);
+    });
+  }
+});
